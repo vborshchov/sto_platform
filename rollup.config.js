6b233694f38a15e0275a6706df86fc7e9a3663fb
Victor Borshchov <victor.borshchov@gmail.com>
2019-05-21 22:58:04 +0300

    add tailwindcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,45 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import purgeCss from '@fullhuman/postcss-purgecss'
+import autoprefixer from 'autoprefixer'
+import postcssImport from 'postcss-import'
+import commonjs from 'rollup-plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import resolve from 'rollup-plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
+import svelte from 'rollup-plugin-svelte'
+import { terser } from 'rollup-plugin-terser'
+import tailwind from 'tailwindcss'
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
+const removeUnusedCss = purgeCss({
+  content: ['./src/**/*.html', './src/**/*.svelte'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/bundle.css');
-			}
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve(),
-		commonjs(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
-};
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/bundle.js',
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      emitCss: true,
+    }),
+    postcss({
+      plugins: [
+        postcssImport,
+        tailwind(),
+        autoprefixer,
+        production && removeUnusedCss,
+      ].filter(Boolean),
+      extract: 'public/bundle.css',
+    }),
+    resolve(),
+    commonjs(),
+    !production && livereload('public'),
+    production && terser(),
+  ],
+}
--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
-	export let name;
+  import Button from './Button.svelte'
+
+  export let name
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
+<style lang="postcss">
+  @import 'tailwindcss/base';
+  @import 'tailwindcss/components';
+  @import 'tailwindcss/utilities';
+
+  h1 {
+    @apply bg-orange-500 text-white;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<main>
+  <h1 class="text-2xl">Hello {name}!</h1>
+  <Button />
+</main>
